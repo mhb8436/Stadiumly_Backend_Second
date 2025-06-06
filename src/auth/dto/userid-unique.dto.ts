103d5247621ef8158b92d8cf4d322c74202d407c
pepperflavor <j8747j@gmail.com>
2025-06-06 14:51:28 +0900

    밸리데이터 수정

--- src/auth/dto/userid-unique.dto.ts
+++ src/auth/dto/userid-unique.dto.ts
@@ -3,7 +3,6 @@ import { IsEmail, IsString, Matches } from 'class-validator';
 
 export class CheckUniqueUserIdDto {
   @IsString()
-  @IsEmail()
   @Matches(/^[a-zA-Z0-9]{4,12}$/, {
     message: '닉네임은 영문 대소문자와 숫자로만 이루어진 4~12자여야 합니다.',
   })
