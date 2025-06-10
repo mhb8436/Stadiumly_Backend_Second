059a4baf17eb61751d5b6924898d896147c6ec1d
pepperflavor <j8747j@gmail.com>
2025-06-10 20:09:51 +0900

    구내식당 컨트롤러 수정

--- src/cafeteria/cafeteria.controller.ts
+++ src/cafeteria/cafeteria.controller.ts
@@ -6,24 +6,21 @@ import { ApiParam, ApiQuery } from '@nestjs/swagger';
 export class CafeteriaController {
   constructor(private readonly cafeteriaService: CafeteriaService) {}
 
-  @Get('/:sta_id')
+  @Get('/:sta_id') // URL 파라미터 경로 추가
   @ApiParam({
-    name: 'sta_Id',
-    description: '노션 백엔드 요청 구장 아이디 문서 참고 (예: 7)',
-    example: '7 - 잠실야구장 : Stadium id 참고',
+    name: 'sta_id',
+    description: '구장 아이디, ex: 7',
+    required: true,
   })
   @ApiQuery({
     name: 'location',
-    description: '노션 백엔드 요청 구장 아이디 문서 참고 (예: 3ru)',
-    example: '3ru : Stadium location참고',
+    description: '구장 내 위치, ex: 3ru',
     required: true,
   })
   async getCafeteriaListById(
     @Param('sta_id') sta_id: string,
     @Query('location') location: string,
   ) {
-    console.log('카페테리아 컨트롤렁 : ');
-    console.log(sta_id, location);
     return await this.cafeteriaService.getCafeList(sta_id, location);
   }
 }
